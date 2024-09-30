8ab43a2f5189964e094a6a8d02bfb2238e81d960
Scratchy1024 <funnnnnyyyyy@gmail.com>
2024-09-29 19:52:58 -0500

    Update index.js

--- Gleaming Depths PTS Patch Notes/index.js
+++ Gleaming Depths PTS Patch Notes/index.js
@@ -2,13 +2,13 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "inline-block") {
+    if (content.style.display === "block") {
       content.style.display = "none";
     } else {
-      content.style.display = "inline-block";
+      content.style.display = "block";
     }
   });
 }
