f61912e6c3e5f8494cb2025cf68021f772183a0c
Scratchy1024 <funnnnnyyyyy@gmail.com>
2024-09-29 20:06:39 -0500

    Update index.js

--- Gleaming Depths PTS Patch Notes/index.js
+++ Gleaming Depths PTS Patch Notes/index.js
@@ -2,6 +2,11 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
+    var content = coll[i].nextElementSibling;
+    content.style.display = "none";
+};
+  
+for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
