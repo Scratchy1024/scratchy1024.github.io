571a49992afb6406ba9a68a540469244124071c9
Scratchy1024 <funnnnnyyyyy@gmail.com>
2024-09-30 00:37:33 -0500

    Added arrows to collapsibles

--- Gleaming Depths PTS Patch Notes/index.js
+++ Gleaming Depths PTS Patch Notes/index.js
@@ -1,14 +1,29 @@
 var coll = document.getElementsByClassName("collapsible");
+var triDiv = document.createElement('i');
+triDiv.classList.add('fa');
+triDiv.classList.add('fa-caret-right');
+triDiv.classList.add('fa-2x');
+triDiv.classList.add('triangle');
 var i;
+let hovercounters = [];
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].insertBefore(triDiv.cloneNode(true), coll[i].firstChild);
+}
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
+    var collapsibleTriangle = this.firstElementChild;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
+      collapsibleTriangle.classList.remove('fa-caret-down');
+      collapsibleTriangle.classList.add('fa-caret-right');
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
+      collapsibleTriangle.classList.remove('fa-caret-right');
+      collapsibleTriangle.classList.add('fa-caret-down');
     }
   });
 }
